cf92e3ae98697653f95333c11661a23a3dd1d10d
chohw9798 <44824653+chohw9798@users.noreply.github.com>
2018-11-14 11:18:52 +0900

    Add files via upload

---
+++ Mondrian_CompositionA.js
@@ -0,0 +1,324 @@
+var x,y,w,h;
+function setup()
+{
+  background(255);
+  createCanvas(570,570);
+  strokeWeight(3);
+  
+}
+
+function draw()
+{
+  stroke(30,30,30);
+  
+  //1
+  x=0,y=0,w=40,h=140;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(0);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+  
+  //2
+  x=40,y=0,w=200,h=20;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(219,222,229);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+  
+  //3
+  x=40,y=20,w=90,h=120;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(240,60,43);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+  
+  //4
+  x=130,y=20,w=110,h=120;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(240,250,249);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+  
+  //5
+  x=0, y=140, w=40, h=210;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(240,250,249);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+  
+  //6
+  x=40, y=140, w=200, h=110;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(0,70,182);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+  
+  //7
+  x=40, y=250, w=200, h=100;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(0);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h); 
+  
+  //8
+  x=0,y=350,w=100,h=140;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(219,222,229);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+ 
+  //9
+  x=0, y=490, w=100, h=80;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(240,250,249);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //10
+  x=100, y=350, w=140, h=180;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(240,60, 43); 
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //11
+  x=100, y=530, w=270, h=40;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(0);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //12
+  x=240, y=0, w=130, h=90;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(240,250,249);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //13
+  x=240, y=90, w=130, h=50;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(254,241,4);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //14
+  x=240, y=140, w=130, h=110;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(254,241,4);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //15
+  x=240, y=250, w=130, h=100;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(240,250,249);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //16
+  x=240, y=350, w=130, h=90;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(240,250,249);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+  
+  //17
+  x=240, y=440, w=130, h=90;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(0,70,182); 
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //18
+  x=370, y=0, w=130, h=90;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(219,222,229);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //19
+  x=370, y=90, w=130, h=50;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(0,70,182); 
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //20
+  x=370, y=140, w=130, h=110;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(219,222,229);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //21
+  x=370, y=250, w=130, h=190;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(0);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //22
+  x=370, y=440, w=130, h=90;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(254,241,4);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //23
+  x=370, y=530, w=130, h=40;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(219,222,229);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //24
+  x=500, y=0, w=70, h=140;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(254,241,4); 
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //25
+  x=500, y=140, w=35, h=300;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(240,60,43);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //26
+  x=535, y=140, w=35, h=300;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(240,250,249);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+
+  //27
+  x=500, y=440, w=70, h=130;
+  if(mouseIsPressed==true && mouseButton==LEFT){
+  F();
+  }
+  else fill(240,250,249);
+  if(keyIsPressed==true && (0<=key<=9)){
+    R(key);         
+  }
+  else rect(x,y,w,h);
+}
+
+
+function F(){
+fill(random(255),random(255),random(255));
+}
+
+function R(key){
+push();                                
+  translate(x+w/2, y+h/2);                       
+  rotate(radians(key*10));
+  rectMode(CENTER);
+  rect(0,0,w,h);                
+  pop(); 
+}
